d9cc77b20249183dd5c788840b1cea7d0ac38544
teeerapong2020 <teerapong.chue@gmail.com>
2024-05-13 15:32:52 +0700

    finsih

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const dayNumber=6;
+if(dayNumber.length >= 6){
+    console.log('day')
+}else if(dayNumber == 0){
+    console.log('sunday')
+}else if(dayNumber == 1){
+    console.log('monday')
+}else if(dayNumber == 2){
+    console.log('tuesday')
+}else if(dayNumber == 3){
+    console.log('wedesday')
+}else if(dayNumber == 4){
+    console.log('thursday')
+}else if(dayNumber == 5){
+    console.log('friday')
+}else if(dayNumber == 6){
+    console.log('saturday')
+}
+else{
+    console.log('Invalid day number')
+};
